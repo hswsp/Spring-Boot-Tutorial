51c19d785b6f017f116a62bbee32e306efe6c533
hswsp <hswsp@mail.ustc.edu.cn>
2021-11-09 20:27:38 +0800

    修改框架

--- demo/src/main/java/com/springboot/controller/HtmlEmailController.java
+++ demo/src/main/java/com/springboot/controller/HtmlEmailController.java
@@ -16,54 +16,54 @@ import javax.mail.internet.MimeMessage;
 @RequestMapping("/email")
 public class HtmlEmailController {
 
-    @Autowired
-    private JavaMailSender jms;
-
-    @Value("${spring.mail.username}")
-    private String from;
-
-    @Autowired
-    private TemplateEngine templateEngine;
+//    @Autowired
+//    private JavaMailSender jms;
+//
+//    @Value("${spring.mail.username}")
+//    private String from;
+//
+//    @Autowired
+//    private TemplateEngine templateEngine;
 
     @RequestMapping("sendHtmlEmail")
     public String sendHtmlEmail() {
-        MimeMessage message = null;
-        try {
-            message = jms.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom(from);
-            helper.setTo("shengping@ustc.edu"); // 接收地址
-            helper.setSubject("一封HTML格式的邮件"); // 标题
-            // 带HTML格式的内容
-            StringBuffer sb = new StringBuffer("<p style='color:#6db33f'>使用Spring Boot发送HTML格式邮件。</p>");
-            helper.setText(sb.toString(), true);
-            jms.send(message);
+//        MimeMessage message = null;
+//        try {
+//            message = jms.createMimeMessage();
+//            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//            helper.setFrom(from);
+//            helper.setTo("shengping@ustc.edu"); // 接收地址
+//            helper.setSubject("一封HTML格式的邮件"); // 标题
+//            // 带HTML格式的内容
+//            StringBuffer sb = new StringBuffer("<p style='color:#6db33f'>使用Spring Boot发送HTML格式邮件。</p>");
+//            helper.setText(sb.toString(), true);
+//            jms.send(message);
             return "发送成功";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.getMessage();
-        }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return e.getMessage();
+//        }
     }
 
     @RequestMapping("sendTemplateEmail")
     public String sendTemplateEmail(String code) {
         MimeMessage message = null;
-        try {
-            message = jms.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom(from);
-            helper.setTo("shengping@ustc.edu"); // 接收地址
-            helper.setSubject("邮件摸板测试"); // 标题
-            // 处理邮件模板
-            Context context = new Context();
-            context.setVariable("code", code);
-            String template = templateEngine.process("emailTemplate", context);
-            helper.setText(template, true);
-            jms.send(message);
+//        try {
+//            message = jms.createMimeMessage();
+//            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//            helper.setFrom(from);
+//            helper.setTo("shengping@ustc.edu"); // 接收地址
+//            helper.setSubject("邮件摸板测试"); // 标题
+//            // 处理邮件模板
+//            Context context = new Context();
+//            context.setVariable("code", code);
+//            String template = templateEngine.process("emailTemplate", context);
+//            helper.setText(template, true);
+//            jms.send(message);
             return "发送成功";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.getMessage();
-        }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return e.getMessage();
+//        }
     }
 }
