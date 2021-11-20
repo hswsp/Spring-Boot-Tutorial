9328720939887c8a0fdea61e8c828a5617ee1085
hswsp <hswsp@mail.ustc.edu.cn>
2021-11-20 13:15:38 +0800

    tutorial 30

--- demo/src/main/java/com/springboot/listener/KafkaMessageListener.java
+++ demo/src/main/java/com/springboot/listener/KafkaMessageListener.java
@@ -4,7 +4,6 @@ import com.springboot.model.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.PartitionOffset;
 import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -19,11 +18,7 @@ public class KafkaMessageListener {
     @KafkaListener(groupId = "test-consumer",
             topicPartitions = @TopicPartition(topic = "test", partitions = { "0", "1" }))
 
-//    @KafkaListener(groupId = "test-consumer",
-//        topicPartitions = @TopicPartition(topic = "test",
-//                partitionOffsets = {
-//                        @PartitionOffset(partition = "0", initialOffset = "0")
-//                }))
+// 1
     public void listen(@Payload Message message,
                        @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
         logger.info("接收消息: {}，partition：{}", message, partition);
